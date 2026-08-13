b194d733a038683809c59585857e83fdaa1b8f46
RanzCloud <zerigamim@gmail.com>
2026-08-13 19:33:09 +0700

    Hapus env.ts

--- website-- / backend/src/config/env.ts
+++
@@ -1,2 +0,0 @@
-import dotenv from "dotenv"; dotenv.config();
-export const env={port:Number(process.env.PORT??3000),jwtSecret:process.env.JWT_SECRET!,frontendUrl:process.env.FRONTEND_URL??"http://localhost:3001"};
